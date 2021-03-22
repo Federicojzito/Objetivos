dda724155efb8448389beb82471f48247c8b8738
Federico Zito <federicojzito@gmail.com>
2021-03-22 05:35:38 -0300

    Modificando Array

--- Objetivo1/js/app.js
+++ Objetivo1/js/app.js
@@ -10,7 +10,7 @@ document.querySelector("#btn-add").addEventListener('click', function () {
     // Validate
     if (name.value.length === 0 || producing_company.value.length === 0 || !parseInt(price.value)) return;
 
-    var user = {
+    var product = {    
         name: name.value,
         producing_company: producing_company.value,
         price: price.value
@@ -22,37 +22,37 @@ document.querySelector("#btn-add").addEventListener('click', function () {
     price.value = '';
 
     // Append to my localStorage
-    appendObjectToLocalStorage(user);
+    appendObjectToLocalStorage(product);
 })
 
 function appendObjectToLocalStorage(obj) {
-    var users = [],
+    var products = [],
         dataInLocalStorage = localStorage.getItem(localStorageKeyName);
 
     if (dataInLocalStorage !== null) {
-        users = JSON.parse(dataInLocalStorage);
+        products = JSON.parse(dataInLocalStorage);
     }
 
-    users.push(obj);
+    products.push(obj);
 
-    localStorage.setItem(localStorageKeyName, JSON.stringify(users));
+    localStorage.setItem(localStorageKeyName, JSON.stringify(products));
 
     loadFromLocalStorage();
 }
 
 function loadFromLocalStorage() {
-    var users = [],
+    var products = [],
         dataInLocalStorage = localStorage.getItem(localStorageKeyName),
         gridBody = document.querySelector("#grid tbody");
 
     if (dataInLocalStorage !== null) {
-        users = JSON.parse(dataInLocalStorage);
+        products = JSON.parse(dataInLocalStorage);
     }
 
     // Draw TR from TBODY
     gridBody.innerHTML = '';
 
-    users.forEach(function (x, i) {
+    products.forEach(function (x, i) {
         var tr = document.createElement("tr"),
             tdName = document.createElement("td"),
             tdProducing_company = document.createElement("td"),
@@ -82,16 +82,16 @@ function loadFromLocalStorage() {
 }
 
 function removeFromLocalStorage(index){
-    var users = [],
+    var products = [],
         dataInLocalStorage = localStorage.getItem(localStorageKeyName);
 
-    users = JSON.parse(dataInLocalStorage);
+    products = JSON.parse(dataInLocalStorage);
 
-    users.splice(index, 1);
+    products.splice(index, 1);
 
-    localStorage.setItem(localStorageKeyName, JSON.stringify(users));
+    localStorage.setItem(localStorageKeyName, JSON.stringify(products));
 
     loadFromLocalStorage();
     
-    users.sort(users);
+    products.sort(products);
 }
