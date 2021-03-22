5e84c93951abe08056a27a0253529e8b641cd949
Federico Zito <federicojzito@gmail.com>
2021-03-22 15:52:26 -0300

    Tic Tac Toe

---
+++ Ejercicios-OBJ1/TicTacToe/js/app.js
@@ -0,0 +1,117 @@
+// CONSTANTS
+
+const STATUS_DISPLAY = document.querySelector('.game-notification'),
+GAME_STATE = ['','','','','','','','',''],
+WINNINGS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+],
+WIN_MESSAGE = () => `This Player ${currentPlayer} has won!`,
+DRAW_MESSAGE = () => `The Game ended in a draw`,
+CURRENT_PLAYER_TURN = () => `Player turn ${currentPlayer}`;
+
+// VARIABLES
+
+let gameActive = true,
+    currentPlayer = 'O';
+
+// FUNCTIONS
+
+
+function startGame() {
+    handleStatusDisplay(CURRENT_PLAYER_TURN());
+    listeners();
+  }
+  
+  function listeners() {
+    document.querySelector('.game-container').addEventListener('click', handleCellClick);
+    document.querySelector('.game-restart').addEventListener('click', handleRestartGame);
+  }
+  
+  function handleStatusDisplay(message) {
+    STATUS_DISPLAY.innerHTML = message;
+  }
+  
+  function handleRestartGame() {
+    gameActive = true;
+    currentPlayer = "X";
+    restartGameState();
+    handleStatusDisplay(CURRENT_PLAYER_TURN());
+    document.querySelectorAll('.game-cell').forEach(cell => cell.innerHTML = "");
+  }
+  
+  function handleCellClick(clickedCellEvent /** Type Event **/) {
+    const clickedCell = clickedCellEvent.target;
+    if (clickedCell.classList.contains('game-cell')) {
+      const clickedCellIndex = Array.from(clickedCell.parentNode.children).indexOf(clickedCell)
+      if (GAME_STATE[clickedCellIndex] !== '' || !gameActive) {
+        return false
+      }
+  
+      handleCellPlayed(clickedCell, clickedCellIndex);
+      handleResultValidation();
+    }
+  }
+  
+  function handleCellPlayed(clickedCell /** object HTML **/, clickedCellIndex) {
+      // Add in the corresponding position the value either "X" or "O" in the current state of the game
+    GAME_STATE[clickedCellIndex] = currentPlayer;   
+    // Add the player's value to the HTML
+    clickedCell.innerHTML = currentPlayer;
+  }
+  
+  function handleResultValidation() {
+    let roundWon = false;
+    for (let i = 0; i < WINNINGS.length; i++) {  
+    // Iterate each of the possible winning combinations
+      const winCondition = WINNINGS[i];
+    // Save the combination for example: [0, 1, 2]
+      let position1 = GAME_STATE[winCondition[0]],
+        position2 = GAME_STATE[winCondition[1]],
+        position3 = GAME_STATE[winCondition[2]]    
+    // Stores the value of the current state of the game according to the positions of winCondition
+  
+      if (position1 === '' || position2 === '' || position3 === '') {
+        continue; // If there is any empty value no one has won yet
+      }
+      if (position1 === position2 && position2 === position3) {
+        roundWon = true // If all the positions match then, that player has won the game
+        break;
+      }
+    }
+  
+    if (roundWon) {
+      handleStatusDisplay(WIN_MESSAGE());
+      gameActive = false;
+      return;
+    }
+  
+    let roundDraw = !GAME_STATE.includes("") ;// If all cells have value and the previous statement was false then it is a tie
+    if (roundDraw) {
+      handleStatusDisplay(DRAW_MESSAGE());
+      gameActive = false;
+      return;
+    }
+  
+    handlePlayerChange();
+  }
+  
+  function handlePlayerChange() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    handleStatusDisplay(CURRENT_PLAYER_TURN());
+  }
+  
+  function restartGameState() {
+    let i = GAME_STATE.length;
+    while (i--) {
+      GAME_STATE[i] = ''
+    }
+  }
+  
+  startGame();
